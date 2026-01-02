7665c4e1050551dc676e47e3d0c185cdadcac364
chiway_Luo <chiwayfpv@gmail.com>
2026-01-03 00:15:03 +0800

    新增练习9的动态参数

--- src/my_exercise/my_exer09_tf_pub/launch/stage_ros2_multi.launch.py
+++ src/my_exercise/my_exer09_tf_pub/launch/stage_ros2_multi.launch.py
@@ -17,10 +17,11 @@ import os
 # from launch.actions import ExecuteProcess
 # from launch.substitutions import FindExecutable   #FindExecutable(name="ros2")
 # 参数声明与获取-----------------
-# from launch.actions import DeclareLaunchArgument
-# from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 # from launch.conditions import IfCondition #判断是否执行
 # from launch.conditions import UnlessCondition #取反
+from launch.substitutions import PythonExpression
 # 文件包含相关-------------------
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -39,6 +40,11 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     ld = LaunchDescription()
 
+    #声明参数
+    # middle_distance = 0.5#仿真地图中半个格子的距离
+    middle_distance = DeclareLaunchArgument(name="middle_distance",default_value="0.5")
+    ld.add_action(middle_distance)
+
     #包含stage_ros2中的my_house_multi.launch.py
     stage_ros2_multi = IncludeLaunchDescription(
         launch_description_source=PythonLaunchDescriptionSource(
@@ -53,14 +59,27 @@ def generate_launch_description():
         package="tf2_ros",
         executable="static_transform_publisher",
         name="robot0_to_map_broadcaster",
-        arguments=["--frame-id","map","--child-frame-id","robot_0/odom","--x","1","--y","1","--yaw","0.7854"]
+        # arguments=["--frame-id","map","--child-frame-id","robot_0/odom","--x",str(middle_distance),"--y",str(middle_distance),"--yaw","0.7854"]
+        arguments=[
+            "--frame-id","map",
+            "--child-frame-id","robot_0/odom",
+            "--x",LaunchConfiguration("middle_distance"),
+            "--y",LaunchConfiguration("middle_distance"),
+            "--yaw","0.7854"
+        ]
     )
 
     robot1_to_map = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
         name="robot1_to_map_broadcaster",
-        arguments=["--frame-id","map","--child-frame-id","robot_1/odom","--x","3","--y","3"]
+        # arguments=["--frame-id","map","--child-frame-id","robot_1/odom","--x",str(2 + middle_distance),"--y",str(2 + middle_distance)]
+        arguments=[
+            "--frame-id","map",
+            "--child-frame-id","robot_1/odom",
+            "--x",PythonExpression(['str(2 + float(',LaunchConfiguration("middle_distance"),'))']),
+            "--y",PythonExpression(['str(2 + float(',LaunchConfiguration("middle_distance"),'))'])
+        ]
     )
     ld.add_action(robot_0_to_map)
     ld.add_action(robot1_to_map)
