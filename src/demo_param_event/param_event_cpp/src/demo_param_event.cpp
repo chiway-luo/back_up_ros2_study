0e9debb5b276152d484236ea3e636c8de0ca6dd9
chiway_Luo <chiwayfpv@gmail.com>
2026-01-04 01:04:20 +0800

    准备功能包架构

---
+++ src/demo_param_event/param_event_cpp/src/demo_param_event.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world param_event_cpp package\n");
+  return 0;
+}
---
+++ src/demo_plugin/planner_sample/src/planner_sample.cpp
@@ -0,0 +1,14 @@
+#include "planner_sample/planner_sample.hpp"
+
+namespace planner_sample
+{
+
+PlannerSample::PlannerSample()
+{
+}
+
+PlannerSample::~PlannerSample()
+{
+}
+
+}  // namespace planner_sample
