e8491e44ed36705bd34fecdf017adf6cb78caf26
chiway_Luo <chiwayfpv@gmail.com>
2026-01-11 02:39:08 +0800

    完成odom2path案例(python)

---
+++ src/demo/py00_odom/launch/odom2path.launch.py
@@ -0,0 +1,77 @@
+#install(DIRECTORY config params launch DESTINATION share/${PROJECT_NAME}) #cmake配置
+#<exec_depend>ros2launch</exec_depend> <!--package.xml配置-->
+#from glob import glob #用于setup.py配置多个launch文件
+#('share/' + package_name, glob('launch/*_launch.py')),
+#('share/' + package_name, glob('launch/*_launch.xml')),
+#('share/' + package_name, glob('launch/*_launch.yaml')),
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+# 封装终端指令相关类--------------
+# from launch.actions import ExecuteProcess
+# from launch.substitutions import FindExecutable   #FindExecutable(name="ros2")
+# 参数声明与获取-----------------
+# from launch.actions import DeclareLaunchArgument
+# from launch.substitutions import LaunchConfiguration
+# from launch.conditions import IfCondition #判断是否执行
+# from launch.conditions import UnlessCondition #取反
+# from launch.substitutions import PythonExpression #运行时计算表达式
+# 文件包含相关-------------------
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+# 分组相关----------------------
+# from launch_ros.actions import PushRosNamespace
+# from launch.actions import GroupAction
+# 事件相关----------------------
+# from launch.event_handlers import OnProcessStart, OnProcessExit
+# from launch.actions import ExecuteProcess, RegisterEventHandler,LogInfo
+# 获取功能包下share目录路径-------
+from ament_index_python.packages import get_package_share_directory
+# urdf文件处理相关--------------
+# from launch_ros.parameter_descriptions import ParameterValue
+# from launch.substitutions import Command
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    my_rviz_config = os.path.join(
+        get_package_share_directory('py00_odom'),
+        # 'config',
+        'odom2path.rviz'
+    )
+    
+
+    #stage仿真环境
+    stage_launch = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('stage_ros2'),
+                'launch',
+                'my_house.launch.py'
+            )
+        ),
+        launch_arguments={
+            'rviz_config': my_rviz_config
+        }.items()
+    )
+    ld.add_action(stage_launch)
+
+    #里程计转换路径节点
+    odom2path_node = Node(
+        package='py00_odom',
+        executable='odom2path_py',
+        name='odom2path',
+        output='screen',
+        # parameters=[
+        #     {'path_frame_id': 'odom'},
+        #     {'path_topic_name': '/odom_path'},
+        #     {'odom_topic_name': '/odom'},
+        #     {'publish_rate': 1.0},
+        # ]
+    )
+    ld.add_action(odom2path_node)
+
+    return ld
+
+
--- src/demo/py00_odom/py00_odom/odom2path_py.py
+++ src/demo/py00_odom/py00_odom/odom2path_py.py
@@ -1,6 +1,97 @@
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Path
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose, PoseStamped
+from builtin_interfaces.msg import Time
+"""
+    需求:相比于cpp版本,py中只实现无限制发布路径实现
+    流程:
+        1.包含头文件
+        2.初始化ros2客户端
+        3.自定义节点类
+            3-1创建里程计订阅节点(绑定回调函数)
+            3-2创建路径数据发布节点
+            3-3在回调中转换数据并发布路径数据
+        4.调用spin函数,并传入节点对象指针
+        5.释放资源
+"""
+
+class Odom2Path(Node):
+    def __init__(self,str1):
+        super().__init__(str1)
+        self.get_logger().info(f"成功创建节点{str1}")
+        #创建订阅节点
+        self.sub_ = self.create_subscription(
+            msg_type=Odometry,
+            topic="odom",
+            callback=self.call_back,
+            qos_profile=10
+        )
+        #创建发布对象
+        self.pub_ = self.create_publisher(
+            msg_type=Path,
+            topic="path",
+            qos_profile=10
+        )
+        #创建路径对象
+        self.path_msg_ = Path()
+        #创建最后位姿信息
+        self.last_pose_ = PoseStamped()
+
+    def call_back(self,odom_msg: Odometry):#回调函数
+        #给最后位姿信息赋值
+        self.last_pose_.header.frame_id = odom_msg.header.frame_id
+        #获取时间对象
+        time = odom_msg.header.stamp
+        #计算距离 阈值0.5m
+        if self.current_distance(odom_msg.pose.pose,time) < 0.5:
+            return
+        #转换数据
+        self.path_msg_.header.frame_id = odom_msg.header.frame_id
+        self.path_msg_.header.stamp = time
+        self._append_pose(odom_msg.pose.pose,time)#添加位姿信息,给last_pose_赋值
+        #发布路径数据
+        self.pub_.publish(self.path_msg_)
+
+
+    def current_distance(self,pose: Pose,time: Time):
+        #判断路径数据是否为空
+        if len(self.path_msg_.poses) == 0:
+            self.last_pose_.header.stamp = time
+            self.last_pose_.pose = pose
+            return 10e9
+        #计算距离
+        dx = pose.position.x - self.last_pose_.pose.position.x
+        dy = pose.position.y - self.last_pose_.pose.position.y
+        distance = (dx**2 + dy**2)**0.5
+        #给last_pose_赋值
+        if distance < 0.5:
+            return 0.0
+        self.last_pose_.header.stamp = time
+        self.last_pose_.pose = pose
+        return distance
+    
+    def _append_pose(self,pose: Pose,time: Time):
+        new_pose = PoseStamped()
+        new_pose.header.frame_id = self.last_pose_.header.frame_id
+        new_pose.header.stamp = time
+        new_pose.pose = pose
+        self.path_msg_.poses.append(new_pose)
+        #给last_pose_赋值
+        self.last_pose_.pose = pose
+        self.last_pose_.header.stamp = time
+        
+
 def main():
-    print('Hi from py00_odom.')
+    #初始化ros2客户端
+    rclpy.init()
 
+    #调用spin函数,传入自定义类对象
+    rclpy.spin(Odom2Path("odom_2_path_py"))
 
-if __name__ == '__main__':
+    #释放资源
+    rclpy.shutdown()
+
+if __name__ == "__main__":
     main()
